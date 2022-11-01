5aeb3603e20b5c71aa781a8f64b0dae5c299e9c6
ROBERT PENROD <derek.robert@uamcr.net>
2022-11-01 15:05:56 -0600

    Add project files.

---
+++ ASPNETMVCCRUD/Data/MCVDemoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MCVDemoDbContext : DbContext   // Inherit from db context
+    {
+        // Constructor
+        public MCVDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Properties, used to access tables
+        public DbSet<Employee> Employees { get; set; }  // Table created will also be name Employees
+
+
+    }
+}
---
+++ ASPNETMVCCRUD/Program.cs
@@ -0,0 +1,35 @@
+using ASPNETMVCCRUD.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(); // Tell services we are using controllers with views
+
+// Inject DbContext into Services
+builder.Services.AddDbContext<MCVDemoDbContext>(options =>
+    )
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Gives Middleware a default route to action=index if we check HomeController it has the Index view
+// that is showed when loading app
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
